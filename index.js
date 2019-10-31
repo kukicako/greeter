3cbc3ff8ae6dda5dba5babf57285b81569fc8f18
Muamer Kukic <kukicako@gmail.com>
2019-10-31 12:59:34 -0700

    deployed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.get('/', (req, res) => {
 
 // the port is coming from the environment
 const port = process.env.PORT || 4000;
-// the PORT will be set by heroku when we deploy
+
 
 server.listen(port, () => {
   console.log(`\n Server running on port ${port} \n`);
