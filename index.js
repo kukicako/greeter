1a9b4f50927acad5752795d4293e42454d946601
Luis Hernandez <luishrd@gmail.com>
2019-10-31 11:48:33 -0600

    respond with message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log('\nmessage:', process.env.MSG);
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+  res.status(200).json({ message: process.env.MSG });
 });
 
 // the port is coming from the environment
